0b4a39cdd6d925d85f3f7154329b58b4f42b78ac
psrit <xiaojx13@outlook.com>
2020-02-02 20:12:42 +0800

    Implement `inner_product` and `normalized_overlap`.

--- paulicirq/utils.py
+++ paulicirq/utils.py
@@ -99,6 +99,50 @@ def random_complex_matrix(*dn) -> np.ndarray:
     return matrix
 
 
+def inner_product(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """
+    Calculates the inner product between vectors a and b, which equals <a|b>.
+
+    :param a:
+        Vector of n-dimensional (n,), or some vectors of n-dimensional (m, n).
+    :param b:
+        Vector of n-dimensional (n,), or some vectors of n-dimensional (m, n).
+    :return:
+        Inner product between |a> and |b>.
+
+    """
+    braket_a_b = a.conjugate() * b
+    if braket_a_b.ndim == 1:
+        braket_a_b = sum(braket_a_b)
+    else:
+        braket_a_b = np.sum(braket_a_b, axis=1)
+    return braket_a_b
+
+
+def normalized_overlap(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """
+    Calculates the overlap between the vectors normalized from a and b.
+
+    The overlap is also called fidelity, which equals |<a'|b'>|^2, where
+    |a'> = a / ||a||, |b'> = b / ||b||.
+
+    :param a:
+        Vector of n-dimensional (n,), or some vectors of n-dimensional (m, n).
+    :param b:
+        Vector of n-dimensional (n,), or some vectors of n-dimensional (m, n).
+    :return:
+        Overlap between |a'> and |b'>.
+
+    """
+    braket_a_b = inner_product(a, b)  # (m,) or ()
+    braket_a_b_squared = braket_a_b * braket_a_b.conjugate()  # |<a|b>|^2
+
+    a_2 = inner_product(a, a)  # ||a||^2, (m,) or ()
+    b_2 = inner_product(b, b)  # ||b||^2, (m,) or ()
+    overlap = braket_a_b_squared / (a_2 * b_2)  # |<a'|b'>|^2
+    return overlap
+
+
 def resolve_scalar(c, param_resolver: cirq.ParamResolverOrSimilarType):
     try:
         _c_resolved = cirq.resolve_parameters(c, param_resolver)
